e82cc3f721d708527081cb15c7b0c699e01b6d7b
aboynejames <james@aboynejames.co.uk>
2022-11-07 21:01:21 +0000

    start of modular HOP refactor

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import SafeFlow from 'node-safeflow'
+import MessageFlow from 'hop-message'
+
+const HOP = new SafeFlow()
+console.log('{{SafeFLOW-ECS}}')
+const MessagesFlow = new MessageFlow()
+console.log(MessageFlow)
